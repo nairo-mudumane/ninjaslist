c447681e2f59d20ff02ea88821eab16360aae8af
nairoMudumane <nazirmudumane@gmail.com>
2021-12-10 13:31:36 +0200

    first commit

---
+++ pages/about/index.js
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div>
+      <h1>ABout page</h1>
+      <p>
+        Elit incididunt enim reprehenderit dolore irure amet elit. Consequat
+        esse culpa ad laborum velit exercitation nulla nisi. Pariatur aute ut
+        sit pariatur aute et ipsum eu.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/ninjas/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import React from 'react';
+
+export default function Ninjas() {
+  return (
+    <div>
+      <Head>
+        <title>List all.</title>
+      </Head>
+      <h1>All ninjas</h1>
+    </div>
+  );
+}
